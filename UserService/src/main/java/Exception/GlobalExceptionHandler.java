0f4a2bdf56355e428fa021a2296e8a48553ed80c
Rakesh Paswan <vikash25kumarpa@gmail.com>
2025-08-03 16:50:23 +0530

    implement better exceptin with global handler

---
+++ UserService/src/main/java/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,41 @@
+package Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import ErrorResponseHandle.UserErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+	
+	@ExceptionHandler(UserNotFoundException.class)
+	public ResponseEntity<UserErrorResponse>HandleUserNotFound(UserNotFoundException ex,HttpServletRequest request){
+		
+		UserErrorResponse error = new UserErrorResponse(404,ex.getMessage(),request.getRequestURI());
+		
+		return ResponseEntity.status(404).body(error);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<UserErrorResponse>HandleValidationError(MethodArgumentNotValidException ex, HttpServletRequest request){
+		
+		UserErrorResponse error = new UserErrorResponse(400, ex.getMessage(), request.getRequestURI());
+		
+		return ResponseEntity.status(400).body(error);
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<UserErrorResponse>OtherExceptin(Exception ex,HttpServletRequest request){
+		
+		UserErrorResponse error = new UserErrorResponse(500, ex.getMessage(), request.getRequestURI());
+		
+		return ResponseEntity.status(500).body(error);
+	}
+	
+	
+}
--- UserService/src/main/java/com/user/UserService/model/User.java
+++ UserService/src/main/java/com/user/UserService/model/User.java
@@ -4,13 +4,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class User {
 	
 	@Id
 	private int id;
+	
+	@NotBlank(message="Name is required")
 	private String name;
+	
+	@NotBlank(message = "email is required")
 	private String email;
 	
 	
